660acc24651c8a5650a380742ba2763b9822ec0c
maylique <supercoder@AirvonSperCoder.fritz.box>
2024-01-05 15:58:08 +0100

    added infinite scroll

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,30 +1,43 @@
-const section = document.querySelector('section')
+const section = document.querySelector('section');
+let i = 1;
+let itemDiv = document.createElement('div');
+let loadBtn = document.createElement('button');
 
-fetch('https://picsum.photos/v2/list')
-.then((resp) =>  resp.json())
-// .then((data) => console.log(data))
-.then((data => {
-    console.log(data);
+function processData(data, i) {
     data.forEach(obj => {
-        const imgUrl = obj.download_url
-        const caption = obj.author
-        const seeMore = obj.url
+        const imgUrl = obj.download_url;
+        const caption = obj.author;
+        const seeMore = obj.url;
 
-        let createFigure = document.createElement('figure')
-        let createImg = document.createElement('img')
-        let createFigCaption = document.createElement('figcaption')
-        let btn = document.createElement('button')
+        let createFigure = document.createElement('figure');
+        let createImg = document.createElement('img');
+        let createFigCaption = document.createElement('figcaption');
+        let btn = document.createElement('button');
 
-        section.appendChild(createFigure)
-        createImg.setAttribute('src', imgUrl)
-        createFigure.appendChild(createImg)
-        createFigure.appendChild(createFigCaption)
-        createFigure.appendChild(btn)
-        createFigCaption.textContent = caption
-        btn.textContent = 'See More'
-        btn.addEventListener('click', () => window.open(seeMore))
-        
+        section.appendChild(createFigure);
+        createImg.setAttribute('src', imgUrl);
+        createFigure.appendChild(createImg);
+        createFigure.appendChild(createFigCaption);
+        createFigure.appendChild(btn);
+        createFigCaption.textContent = caption;
+        btn.textContent = 'See More';
+        btn.addEventListener('click', () => window.open(seeMore));
     });
-}))
-.catch((error) => console.log('Monte dein Aquarium brennt', error))
+    document.querySelector("body").appendChild(itemDiv);
+    document.querySelector("body").appendChild(loadBtn);
+    window.onscroll = function () {
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+            fetch(`https://picsum.photos/v2/list?page=${i}`)
+                .then((resp) => resp.json())
+                .then((data) => processData(data, i))
+                .catch((error) => console.log('Monte dein Aquarium brennt', error));
+        }
+    }
+}
 
+
+
+fetch(`https://picsum.photos/v2/list?page=${i}`)
+    .then((resp) => resp.json())
+    .then((data) => processData(data, i))
+    .catch((error) => console.log('Monte dein Aquarium brennt', error));
