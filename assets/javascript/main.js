44e675e6b2c24a0442e21943a5630ad2eba538b0
maylique <supercoder@AirvonSperCoder.fritz.box>
2024-01-05 12:14:10 +0100

    phase 2 complete

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,23 @@
+const section = document.querySelector('section')
+
 fetch('https://picsum.photos/v2/list')
 .then((resp) =>  resp.json())
-.then((data) => console.log(data))
+// .then((data) => console.log(data))
+.then((data => {
+    console.log(data);
+    data.forEach(obj => {
+        const imgUrl = obj.download_url
+        const caption = obj.author
+
+        let createFigure = document.createElement('figure')
+        let createImg = document.createElement('img')
+        let createFigCaption = document.createElement('figcaption')
+
+        section.appendChild(createFigure)
+        createImg.setAttribute('src', imgUrl)
+        createFigure.appendChild(createImg)
+        createFigure.appendChild(createFigCaption)
+        createFigCaption.textContent = caption
+    });
+}))
 .catch((error) => console.log('Monte dein Aquarium brennt', error))
