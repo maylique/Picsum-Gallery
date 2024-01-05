eaff153b11f538fc998b6bf523d378ad41ed156c
maylique <supercoder@AirvonSperCoder.fritz.box>
2024-01-05 13:42:36 +0100

    added button, added aspectratio

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -8,16 +8,23 @@ fetch('https://picsum.photos/v2/list')
     data.forEach(obj => {
         const imgUrl = obj.download_url
         const caption = obj.author
+        const seeMore = obj.url
 
         let createFigure = document.createElement('figure')
         let createImg = document.createElement('img')
         let createFigCaption = document.createElement('figcaption')
+        let btn = document.createElement('button')
 
         section.appendChild(createFigure)
         createImg.setAttribute('src', imgUrl)
         createFigure.appendChild(createImg)
         createFigure.appendChild(createFigCaption)
+        createFigure.appendChild(btn)
         createFigCaption.textContent = caption
+        btn.textContent = 'See More'
+        btn.addEventListener('click', () => window.open(seeMore))
+        
     });
 }))
 .catch((error) => console.log('Monte dein Aquarium brennt', error))
+
