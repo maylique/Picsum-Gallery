de46af07d72e542e8dc137d37ebf6f3c7259cdfa
maylique <maylique14@gmail.com>
2024-01-05 18:23:12 +0100

    hm failed loading optimizations

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,14 +3,34 @@ let i = 1;
 let itemDiv = document.createElement('div');
 let loadBtn = document.createElement('button');
 
+// const gridColumns = 6;
+// const screenWidth = window.innerWidth; 
+// const cellWidth = screenWidth / gridColumns;
+// const cellHeight = cellWidth * (9 / 16);
+
+// const desiredWidth = Math.floor(cellWidth);
+// const desiredHeight = Math.floor(cellHeight);
+
+
 function processData() {
-    fetch(`https://picsum.photos/v2/list?page=${i}`)
+    fetch(`https://picsum.photos/v2/list?page=${i}&limit=60`)
         .then((resp) => resp.json())
         .then((data) => {
             data.forEach(obj => {
                 const imgUrl = obj.download_url;
                 const caption = obj.author;
                 const seeMore = obj.url;
+                // console.log(obj.width, obj.height);
+                // const regex = /\/(\d+)\/(\d+)/;
+                // const matches = imgUrl.match(regex);
+                // if (matches) {
+                //     const oldWidth = matches[1];
+                //     const oldHeight = matches[2];
+                //     const newWidth = desiredWidth
+                //     const newHeight = desiredHeight
+                //     const modifiedUrl = imgUrl.replace(`${oldWidth}/${oldHeight}`, `${newWidth}/${newHeight}`);
+                //     return modifiedUrl
+                // }
 
                 let createFigure = document.createElement('figure');
                 let createImg = document.createElement('img');
